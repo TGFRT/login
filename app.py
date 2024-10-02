f205c1c661c1e217da52ac68de6795d8f4c827d1
TGFRT <68918738+TGFRT@users.noreply.github.com>
2024-10-02 02:16:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ st.title("Encuesta de Registro")
 # URL del Google Form
 form_url = "https://docs.google.com/forms/d/e/1FAIpQLSc1PGpaEm37ns8m-aXN0XJx03FLLphVGIXY_7N_xPLO0_hC-A/viewform?usp=sf_link"
 
-# Estilos CSS para las franjas y la barra
+# Estilos CSS para la barra
 st.markdown("""
     <style>
         .barra {
-            background-color: rgba(255, 255, 255, 0.8);  /* Color blanco con opacidad */
+            background-color: #ffffff;  /* Color blanco sólido */
             height: 50px; /* Altura de la barra */
             position: absolute;
             top: 20px; /* Espacio desde la parte superior */
@@ -28,11 +28,12 @@ st.markdown("""
             justify-content: center;
             font-size: 20px; /* Tamaño de la fuente */
             font-weight: bold; /* Negrita */
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2); /* Sombra opcional para efecto */
         }
         .iframe-container {
             position: relative;
             z-index: 0;  /* Coloca el iframe detrás de las franjas */
-            height: calc(100vh - 50px); /* Ajustar altura del iframe para que quede debajo de la barra */
+            height: calc(100vh - 70px); /* Ajustar altura del iframe para que quede debajo de la barra */
             overflow: hidden; /* Oculta cualquier desbordamiento */
         }
         iframe {
