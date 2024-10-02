444d5051cd44b7d6d8d3cf2b6382f87e080e92a2
TGFRT <68918738+TGFRT@users.noreply.github.com>
2024-10-02 02:01:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ st.title("Encuesta de Registro")
 form_url = "https://docs.google.com/forms/d/e/1FAIpQLSc1PGpaEm37ns8m-aXN0XJx03FLLphVGIXY_7N_xPLO0_hC-A/viewform?embedded=true"
 
 # Incrustar el formulario usando iframe
-st.markdown(f'<iframe src="{form_url}" width="100%" height="600px" frameborder="0">Cargando…</iframe>', unsafe_allow_html=True)
+st.markdown(f'<iframe src="{form_url}" width="640" height="982" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>', unsafe_allow_html=True)
